833a0f6d7d5ea6b0608efcad1ee48718ef02f142
Cesar De la O <cesaradelao86@gmail.com>
2024-05-19 06:18:59 -0600

    Add API endpoints and principal controller
    This commit introduces API endpoints through the api.php file and a new Principal controller. It also integrates the Laravel Sanctum package for authentication and updates the composer dependencies accordingly. Added routes are protected with Sanctum's middleware. Changes have been made to the bootstrap/app.php file to configure the routing.

---
+++ principal/app/Http/Controllers/Principal.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class Principal extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        try{
+            //se crea una nueva orden con una receta aleatoria
+            $infoOrden= Http::post(env('API_ORDERS','192.168.0.101:84').'/api/newOrder');
+
+            //se recuperan los ingredientes y las cantidades a utilizar
+            $InfoIngredientes=Http::post(env('API_RECIPES','192.168.0.101:82').'/api/required/'.$infoOrden->json('recipe_id'));
+
+            //se validan los ingredientes y se compran los faltantes
+            $info= Http::post(env('API_INGREDIENTS','192.168.0.101:83').'/api/Ingredients',[
+                'Ingredients' => $InfoIngredientes->json('Ingredients'),
+                'Required'=> $InfoIngredientes->json('Required')
+            ]);
+
+            return "Orden Creada";
+        }catch (\Exception $e){
+            return $e->getMessage();
+        }
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ principal/routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/newOrder',[\App\Http\Controllers\Principal::class,'create']);
