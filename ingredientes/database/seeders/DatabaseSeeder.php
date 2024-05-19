7411f07541d197a1f4d9401f5f60ee47d1d955c9
Cesar De la O <cesaradelao86@gmail.com>
2024-05-19 01:17:57 -0600

    Remove database migrations and update ingredient seeder
    The commit involves two major changes; first, it removes the User, Cache and Jobs tables migrations from the database as they were no longer needed. Second, it amends the DatabaseSeeder class under the ingredientes project by adding several ingredients to the seeding process.

--- ingredientes/database/seeders/DatabaseSeeder.php
+++ ingredientes/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +17,45 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table('Ingredients')->insert([
+            'name' => 'Tomato',
+            'units' => 10
+        ]);
+        DB::table('Ingredients')->insert([
+            'name' => 'Lemon',
+            'units' => 10
+        ]);
+        DB::table('Ingredients')->insert([
+            'name' => 'Potato',
+            'units' => 10
+        ]);
+        DB::table('Ingredients')->insert([
+            'name' => 'Rice',
+            'units' => 10
+        ]);
+        DB::table('Ingredients')->insert([
+            'name' => 'Ketchup',
+            'units' => 10
+        ]);
+        DB::table('Ingredients')->insert([
+            'name' => 'Lettuce',
+            'units' => 10
+        ]);
+        DB::table('Ingredients')->insert([
+            'name' => 'Onion',
+            'units' => 10
+        ]);
+        DB::table('Ingredients')->insert([
+            'name' => 'Cheese',
+            'units' => 10
+        ]);
+        DB::table('Ingredients')->insert([
+            'name' => 'Meat',
+            'units' => 10
+        ]);
+        DB::table('Ingredients')->insert([
+            'name' => 'Chicken',
+            'units' => 10
         ]);
     }
 }
