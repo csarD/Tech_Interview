5100c2bdc91449d2d0795a709dae8b0747aa41ee
Cesar De la O <cesaradelao86@gmail.com>
2024-05-19 21:45:50 -0600

    Refactor links to use a new Navbar component
    New Navbar component is added to improve site navigation. Links previously scattered throughout the 'Home' function are now consolidated into a single Navbar component, resulting in a cleaner codebase.

---
+++ front/src/shared/components/Navbar.tsx
@@ -0,0 +1,69 @@
+import styles from "@/app/page.module.css";
+
+export default function navbar() {
+    return <div className={styles.grid}>
+        {window.location.pathname!=="/" &&
+        <a
+            href="/"
+            className={styles.card}
+
+            rel="noopener noreferrer"
+        >
+            <h2>
+                Ordenes Activas<span>-&gt;</span>
+            </h2>
+
+        </a>
+        }
+        {window.location.pathname!=="/Historico" &&
+        <a
+            href="/Historico"
+            className={styles.card}
+
+            rel="noopener noreferrer"
+        >
+            <h2>
+                Ordenes <span>-&gt;</span>
+            </h2>
+            <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+        }
+        <a
+            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <h2>
+                Learn <span>-&gt;</span>
+            </h2>
+            <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </a>
+
+        <a
+            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <h2>
+                Templates <span>-&gt;</span>
+            </h2>
+            <p>Explore starter templates for Next.js.</p>
+        </a>
+
+        <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <h2>
+                Deploy <span>-&gt;</span>
+            </h2>
+            <p>
+                Instantly deploy your Next.js site to a shareable URL with Vercel.
+            </p>
+        </a>
+    </div>
+}
