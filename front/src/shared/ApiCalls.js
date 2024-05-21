92681db3026b957073a85b4d0207c3c9dc9709f9
Cesar De la O <cesaradelao86@gmail.com>
2024-05-20 22:35:55 -0600

    Add trailing slashes to API URI paths
    Trailing slashes have been added to all API endpoints in the Uri object in ApiCalls.js. This serves to standardize the API endpoint paths across the entire application.

--- front/src/shared/ApiCalls.js
+++ front/src/shared/ApiCalls.js
@@ -3,12 +3,12 @@ const url="http://54.146.145.20:90"
 
 
 export const uris={
-    'Bodega':url+"/api/Bodega",
-    'Market':url+"/api/Market",
-    'NuevaOrden':url+"/api/newOrder",
-    'updateStatus':url+"/api/validateStatus",
-    'ActiveOrders':url+"/api/ActiveOrders",
-    'Stats':url+"/api/Stats",
-    'Recetas':url+"/api/Recetas",
-    'AllOrders':url+"/api/Orders",
+    'Bodega':url+"/api/Bodega/",
+    'Market':url+"/api/Market/",
+    'NuevaOrden':url+"/api/newOrder/",
+    'updateStatus':url+"/api/validateStatus/",
+    'ActiveOrders':url+"/api/ActiveOrders/",
+    'Stats':url+"/api/Stats/",
+    'Recetas':url+"/api/Recetas/",
+    'AllOrders':url+"/api/Orders/",
 }
